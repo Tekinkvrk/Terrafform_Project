fc12ca902dffc90d9bef9a8b8a4222eb70542d8d
Tekinkvrk <93817021+Tekinkvrk@users.noreply.github.com>
2022-04-12 00:03:04 +0200

    My studies during the Terraform learning process
    My studies during the Terraform learning process

---
+++ Project-2/terraform-aws/variables.tf
@@ -0,0 +1,29 @@
+variable "ec2_name" {
+  default = "oliver-ec2"
+}
+
+variable "ec2_type" {
+  default = "t2.micro"
+}
+
+variable "ec2_ami" {
+  default = "ami-0742b4e673072066f"
+}
+
+variable "s3_bucket_name" {
+  default = "Hasannnn-s3-bucket-variable-addwhateveryouwant"
+}
+
+variable "num_of_buckets" {
+  default = 2
+}
+
+variable "users" {
+  default = ["tekin-00", "tekin-11", "tekin-22"]
+}
+
+
+
+
+
+
